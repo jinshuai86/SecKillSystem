a73760204c9a5ce2e0e3cabb3f5eaa93ae27d420
JinShuai <jinshuai789@gmail.com>
2018-06-05 11:08:18 +0800

    todo perfect req and res

--- src/main/java/com/jinshuai/seckill/common/Message.java
+++ src/main/java/com/jinshuai/seckill/common/Message.java
@@ -4,6 +4,36 @@ package com.jinshuai.seckill.common;
  * @author: JS
  * @date: 2018/6/4
  * @description:
+ * 响应消息
  */
-public class ReponseMessage {
+public class Message {
+
+    private String status;
+    private String statusCode;
+
+    public Message(String status, String statusCode) {
+        this.status = status;
+        this.statusCode = statusCode;
+    }
+
+    /**
+     * 解析成JSON用，下同
+     * */
+    public String getStatus() {
+        return status;
+    }
+
+    public String getStatusCode() {
+        return statusCode;
+    }
+
+    public Message setStatus(String status) {
+        this.status = status;
+        return this;
+    }
+
+    public Message setStatusCode(String statusCode) {
+        this.statusCode = statusCode;
+        return this;
+    }
 }
--- src/main/java/com/jinshuai/seckill/enums/StatusEnum.java
+++ src/main/java/com/jinshuai/seckill/enums/StatusEnum.java
@@ -1,4 +1,46 @@
 package com.jinshuai.seckill.enums;
 
 public enum StatusEnum {
+
+    /**
+     * 秒杀成功
+     * */
+    SUCCESS("SUCCESS","1"),
+
+    /**
+     * 库存不足 秒杀失败
+     * */
+    LOW_STOCKS("FAIL","0"),
+
+    /**
+     * 系统出错 秒杀失败
+     * */
+    SYSTEM_EXCEPTION("FAIL","-1"),
+
+    /**
+     * 请求参数不完整 禁止后续请求
+     * */
+    INCOMPLETE_ARGUMENTS("INCOMPLETE_ARGUMENTS","-2"),
+
+    /**
+     * 其他 秒杀失败
+     * */
+    FAIL("FAIL","999"),
+    ;
+
+    private String status;
+    private String statusCode;
+
+    StatusEnum(String status, String statusCode) {
+        this.status = status;
+        this.statusCode = statusCode;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getStatusCode() {
+        return statusCode;
+    }
 }
