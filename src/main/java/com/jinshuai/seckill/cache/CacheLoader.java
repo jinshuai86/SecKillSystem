9a55d5ca4bf98ab0120cf34497954cb450575706
JinShuai <jinshuai789@gmail.com>
2018-06-11 19:52:10 +0800

    refactor log

--- src/main/java/com/jinshuai/seckill/cache/CacheLoader.java
+++ src/main/java/com/jinshuai/seckill/cache/CacheLoader.java
@@ -1,11 +1,8 @@
 package com.jinshuai.seckill.cache;
 
-import java.util.List;
-
 import com.jinshuai.seckill.dao.ISecKillDao;
 import com.jinshuai.seckill.entity.Product;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import redis.clients.jedis.Jedis;
@@ -13,6 +10,7 @@ import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.Pipeline;
 
 import javax.annotation.PostConstruct;
+import java.util.List;
 
 /**
  * @author: JS
@@ -21,9 +19,8 @@ import javax.annotation.PostConstruct;
  * 初始化缓存
  */
 @Component
+@Slf4j
 public class CacheLoader {
-
-    private final Logger LOGGER = LoggerFactory.getLogger(CacheLoader.class);
 
     @Autowired
     private ISecKillDao secKillDao;
@@ -38,7 +35,9 @@ public class CacheLoader {
     private void initCache() {
         try {
             Jedis jedis = jedisPool.getResource();
+            // 清除旧的缓存
             jedis.flushDB();
+            // 通过管道执行批处理
             Pipeline pipeline = jedis.pipelined();
             List<Product> productList = secKillDao.getAllProducts();
             productList.forEach(product -> {
@@ -46,9 +45,9 @@ public class CacheLoader {
                 pipeline.set("product:" + product.getId() + ":version", String.valueOf(product.getVersion()));
             });
             pipeline.sync();
-            LOGGER.info("商品库存、版本号已加载到缓存中！！！");
+            log.info("商品库存、版本号已加载到缓存中！！！");
         } catch (Exception e) {
-            LOGGER.error("加载商品到缓存出错,正在停止JVM......",e);
+            log.error("加载商品到缓存出错,正在停止JVM......",e);
             System.exit(-1);
         }
 
