48912676edc9d1997bf926f916c04602665ae240
JinShuai <jinshuai789@gmail.com>
2018-06-11 19:53:18 +0800

    refactor exception & log

--- src/main/java/com/jinshuai/seckill/controller/SecKillController.java
+++ src/main/java/com/jinshuai/seckill/controller/SecKillController.java
@@ -1,81 +1,78 @@
 package com.jinshuai.seckill.controller;
 
-import com.jinshuai.seckill.common.Message;
 import com.jinshuai.seckill.enums.StatusEnum;
+import com.jinshuai.seckill.exception.SecKillException;
 import com.jinshuai.seckill.service.ISecKillService;
 import com.jinshuai.seckill.web.request.SecKillRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.jinshuai.seckill.web.response.SecKillResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Required;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
  * @author: JS
  * @date: 2018/6/3
- * @description:
- * 秒杀请求接口
+ * @description: 秒杀请求接口
  */
 @RestController
+@Slf4j
 public class SecKillController {
-
-    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     private ISecKillService secKillService;
 
     /**
      * 乐观锁
-     * */
+     */
     @PostMapping(value = "/optimistic")
-    Message updateStockByOptimisticLock(@RequestBody SecKillRequest secKillRequest) {
+    SecKillResponse updateStockByOptimisticLock(@RequestBody SecKillRequest secKillRequest) {
         // 初始化响应信息
         StatusEnum statusEnum = StatusEnum.INCOMPLETE_ARGUMENTS;
-        Message responseMessage = new Message(statusEnum.getStatus(),statusEnum.getStatusCode());
+        SecKillResponse secKillResponse = new SecKillResponse(statusEnum.getStatus(), statusEnum.getStatusCode());
         // 检查请求参数
         if (secKillRequest == null || secKillRequest.getProductId() == null || secKillRequest.getUserId() == null) {
-            LOGGER.error("请求参数不完整[{}]",secKillRequest.toString());
-            return responseMessage;
+            throw new SecKillException(statusEnum);
         } else {
             // 封装请求参数
-            Map<String,Integer> requestParameter = new HashMap<>();
-            requestParameter.put("productId",secKillRequest.getProductId());
-            requestParameter.put("userId",secKillRequest.getUserId());
+            Map<String, Integer> requestParameter = new HashMap<>();
+            requestParameter.put("productId", secKillRequest.getProductId());
+            requestParameter.put("userId", secKillRequest.getUserId());
             // 更新库存
             statusEnum = secKillService.updateStockByOptimisticLock(requestParameter);
             // 封装响应参数
-            responseMessage.setStatus(statusEnum.getStatus())
+            secKillResponse.setStatus(statusEnum.getStatus())
                     .setStatusCode(statusEnum.getStatusCode());
         }
-        return responseMessage;
+        return secKillResponse;
     }
 
     /**
      * 悲观锁
-     * */
+     */
     @PostMapping("/pessimistic")
-    Message updateStockByPessimisticLock(@RequestBody SecKillRequest secKillRequest) {
+    SecKillResponse updateStockByPessimisticLock(@RequestBody SecKillRequest secKillRequest) {
 
         StatusEnum statusEnum = StatusEnum.INCOMPLETE_ARGUMENTS;
-        Message message = new Message(statusEnum.getStatus(),statusEnum.getStatusCode());
+        SecKillResponse secKillResponse = new SecKillResponse(statusEnum.getStatus(), statusEnum.getStatusCode());
 
         if (secKillRequest == null || secKillRequest.getProductId() == null || secKillRequest.getUserId() == null) {
-            LOGGER.error("请求参数不完整[{}]",secKillRequest.toString());
-            return message;
+            log.error("请求参数不完整[{}]", secKillRequest.toString());
+            return secKillResponse;
         } else {
-            Map<String,Integer> parameterMap = new HashMap<>();
-            parameterMap.put("userId",secKillRequest.getUserId());
-            parameterMap.put("productId",secKillRequest.getProductId());
+            Map<String, Integer> parameterMap = new HashMap<>();
+            parameterMap.put("userId", secKillRequest.getUserId());
+            parameterMap.put("productId", secKillRequest.getProductId());
 
             statusEnum = secKillService.updateStockByPessimisticLock(parameterMap);
-            message.setStatus(statusEnum.getStatus())
+            secKillResponse.setStatus(statusEnum.getStatus())
                     .setStatusCode(statusEnum.getStatusCode());
         }
-        return message;
+        return secKillResponse;
     }
 
 }
