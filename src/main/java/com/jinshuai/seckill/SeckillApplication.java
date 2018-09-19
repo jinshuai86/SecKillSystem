dc75d9e5410937ea2a7802eb97312fcde1559f4e
JinShuai <jinshuai789@gmail.com>
2018-09-19 19:26:05 +0800

    change xml setting to annotation

--- src/main/java/com/jinshuai/seckill/SeckillApplication.java
+++ src/main/java/com/jinshuai/seckill/SeckillApplication.java
@@ -3,8 +3,9 @@ package com.jinshuai.seckill;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @MapperScan("com.jinshuai.seckill.dao")
 public class SeckillApplication {
 
