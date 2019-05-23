ef158af601090f4deb6e84bc65108bea5d5145b7
JinShuai <jinshuai789@gmail.com>
2019-05-23 11:55:52 +0800

    update:add submodule

---
+++ seckill-account/src/test/java/com/jinshuai/seckill/account/SeckillAccountApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jinshuai.seckill.account;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SeckillAccountApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
---
+++ seckill-product/src/test/java/com/jinshuai/seckill/product/SeckillProductApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jinshuai.seckill.product;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SeckillProductApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
